991f9ffa7c4afeb30a557fc2b2cc6ec540e9546f
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-12-27 17:42:44 +0100

    Remove poweredBy middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono'
 import { compress } from 'hono/compress'
 import { logger } from 'hono/logger'
-import { poweredBy } from 'hono/powered-by'
 import { secureHeaders } from 'hono/secure-headers'
 import api from './api'
 import { customLogger } from './logger'
@@ -10,11 +9,12 @@ import middlewares from './middlewares'
 const app = new Hono().basePath('/v1')
 
 app.use('*', compress())
+
 if (process.env.NODE_ENV !== 'test') {
   app.use('*', logger(customLogger))
 }
+
 app.use('*', secureHeaders())
-app.use('*', poweredBy())
 
 app.get('/', (c) =>
   c.json({
