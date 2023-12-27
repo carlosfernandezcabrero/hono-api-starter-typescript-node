50364e4a42f4336500f7812d32a8afa9a0f7cde2
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-12-27 15:20:28 +0100

    Refactor notFound middleware to return a JSON object with a message

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,7 +1,7 @@
 import { Context } from 'hono'
 
 function notFound({ json, req }: Context): Response {
-  return json(new Error(`🔍 - Not Found - ${req.path}`), 404)
+  return json({ message: `🔍 - Not Found - ${req.path}` }, 404)
 }
 
 export default { notFound }
