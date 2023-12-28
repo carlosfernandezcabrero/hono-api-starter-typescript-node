538f83855c5f922c47f391e0ae2d658b2be64ace
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-12-28 11:11:05 +0100

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { compress } from 'hono/compress'
+import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import { secureHeaders } from 'hono/secure-headers'
 import api from './api'
@@ -9,6 +10,7 @@ import middlewares from './middlewares'
 const app = new Hono().basePath('/v1')
 
 app.use('*', compress())
+app.use('*', cors())
 
 if (process.env.NODE_ENV !== 'test') {
   app.use('*', logger(customLogger))
